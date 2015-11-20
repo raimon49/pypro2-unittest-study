709ddc78ca89bb1d29dd20dfa907edfa332d012e
raimon <raimon49@hotmail.com>
2015-11-20 23:47:23 +0900

    Define virtual class as 'MyView'

---
+++ myview.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+class SomeService(object):
+
+    def __init__(self):
+        pass
+
+    def some_method(self):
+        pass
+
+
+class MyView(object):
+
+    def __init__(self, request):
+        self.request = request
+
+    def index(self):
+        s = SomeService()
+        result = s.some_method(**self.request.params)
+
+        return render('index.html', dict(result=result))
