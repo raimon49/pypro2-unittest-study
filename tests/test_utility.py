a9f8e44982f3ce2830f58589c583d4a4a4781a6a
raimon <raimon49@hotmail.com>
2015-11-21 01:17:14 +0900

    Add test for 'is_last_of_month_now' with 'testfixtures'

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import date
-from utility import is_last_of_month
+from testfixtures import Replacer, test_datetime
+from utility import is_last_of_month, is_last_of_month_now
 
 
 def test_is_last_of_month():
@@ -11,3 +12,9 @@ def test_is_last_of_month():
 def test_is_last_of_month_not():
     d = date(2011, 11, 29)
     assert not is_last_of_month(d), "%s" % d
+
+
+def test_is_last_of_month_now():
+    with Replacer() as r:
+        r.replace('utility.datetime', test_datetime(2011, 11, 30))
+        assert is_last_of_month_now()
--- utility.py
+++ utility.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 def is_last_of_month(d):
     return (d + timedelta(1)).day == 1
+
+
+def is_last_of_month_now():
+    return is_last_of_month(datetime.now())
