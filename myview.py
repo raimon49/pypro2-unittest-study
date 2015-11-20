129c9cc99cdbf6ead7812dde556525cb79f4a1bd
raimon <raimon49@hotmail.com>
2015-11-20 23:50:02 +0900

    Support testability

--- myview.py
+++ myview.py
@@ -11,12 +11,14 @@ class SomeService(object):
 
 
 class MyView(object):
+    someservice_cls = SomeService
 
     def __init__(self, request):
         self.request = request
 
     def index(self):
-        s = SomeService()
+        s = self.someservice_cls()
         result = s.some_method(**self.request.params)
+        self.render_context = dict(result=result)
 
-        return render('index.html', dict(result=result))
+        return render('index.html', self.render_context)
